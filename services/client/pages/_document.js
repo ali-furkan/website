a3f965485a13724860dce857cd86e230aed720b2
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-10-24 19:48:01 +0300

    ⚙ Configuration(client): Configured _document file

---
+++ services/client/pages/_document.js
@@ -0,0 +1,21 @@
+import Document, { Html, Main, Head, NextScript } from "next/document";
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return initialProps;
+    }
+    render() {
+        return (
+            <Html lang="en">
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;

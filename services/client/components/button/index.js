341ff4563bf0c6c16226de5c29ad1f218ba2bc0e
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-10-24 19:45:26 +0300

    🎊 Create Component (client): Button

---
+++ services/client/components/button/index.js
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import style from "./style.module.css";
+
+export const Button = ({ children, bgColor, textColor, text, size }) => (
+    <button
+        className={[
+            style.button,
+            `bg-${bgColor}`,
+            `text-${textColor}`,
+            `text-${size}`,
+        ].join(" ")}
+    >
+        {text ?? children}
+    </button>
+);
+
+Button.defaultProps = {
+    bgColor: "secondary-400",
+    textColor: "white",
+    size: "base",
+};
+
+Button.propTypes = {
+    children: PropTypes.any,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
+    size: PropTypes.string,
+    text: PropTypes.string,
+};

544ca0392dd43b18b1d9b7e5dc07336684ef1869
ali-furkqn <mail@alifurkan.codes>
2020-10-24 19:53:23 +0300

    ⚙ Configuration (client): absolute path, app confs and tailwind

---
+++ services/client/config/configuration.js
@@ -0,0 +1,31 @@
+module.exports = {
+    Navbar: {
+        logo: {
+            img: "",
+            text: ["Ali Furkan", "Website"],
+        },
+        links: {
+            About: {
+                text: "About Me",
+                href: "about",
+            },
+            Projects: "projects",
+            Github: "github",
+        },
+    },
+    colors: {
+        primary: {
+            500: "#060606",
+            400: "#181818",
+            300: "#383838",
+            200: "#686868",
+            100: "#909090",
+        },
+        secondary: {
+            500: "#2A1384",
+            400: "#4F2BDF",
+            300: "#8D7ADC",
+            200: "#D4CBF9",
+        },
+    },
+};

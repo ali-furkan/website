b2850529344f1ab12d8cbe4d4a805d14fa88cad7
ali-furkqn <mail@alifurkan.codes>
2020-10-24 19:49:35 +0300

    🎊 Create Component (client): Create Landing Page

--- services/client/pages/index.js
+++ services/client/pages/index.js
@@ -1,7 +1,26 @@
-export default function Home() {
-    return (
-        <>
-            <h1>Hello</h1>
-        </>
-    );
+import MessageLayout from "@layouts/message";
+
+export default MessageLayout({
+    title: "Not Ready yet",
+    Description() {
+        return (
+            <>
+                You can following this working from{" "}
+                <a
+                    className="underline"
+                    href="https://github.com/ali-furkqn/Website"
+                >
+                    github repo
+                </a>
+            </>
+        );
+    },
+});
+
+export async function getServerSideProps() {
+    return {
+        props: {
+            name: "xd",
+        }, // will be passed to the page component as props
+    };
 }

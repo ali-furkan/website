e6190c40555428c7e757760355920e9bab79e90d
ali-furkqn <mail@alifurkan.codes>
2020-10-24 19:38:18 +0300

    ⚙ Next.js Configuration (client)
    Added new redirects

--- services/client/next.config.js
+++ services/client/next.config.js
@@ -1,3 +1,22 @@
 module.exports = {
     cssModules: true,
+    async redirects() {
+        return [
+            {
+                source: "/github",
+                destination: "https://github.com/ali-furkqn",
+                permanent: false
+            },
+            {
+                source: "/p/:p",
+                destination: "/projects/:p",
+                permanent: false
+            },
+            {
+                source: "/i/:image",
+                destination: "https://cdn.alifurkan.codes/i/:image",
+                permanent: false
+            }
+        ]
+    }
 };

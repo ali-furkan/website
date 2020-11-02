67c46f892fc45ee29dd47b7ff725931c12731bf8
ali-furkqn <mail@alifurkan.codes>
2020-11-03 00:32:07 +0300

    🛠 Edit Page (404)

--- services/client/pages/404.js
+++ services/client/pages/404.js
@@ -1,19 +1,22 @@
-import MainLayout from "@layouts/main";
+import MessageLayout from "@layouts/message";
 
-function Home() {
+function Description() {
     return (
         <>
-            <div className="w-full h-full flex items-center justify-center flex-col flex-1">
-                <h1 className="font-extrabold text-4xl">Page not found</h1>
-                <h2>
-                    If you want go back to home page, you can click{" "}
-                    <a className="underline" href="/">
-                        this link
-                    </a>
-                </h2>
-            </div>
+            <h2>
+                If you want go back to home page, you can click{" "}
+                <a className="underline" href="/">
+                    this link
+                </a>
+            </h2>
         </>
     );
 }
 
-export default MainLayout(Home);
+export default MessageLayout({
+    title: "Page not found",
+    options: {
+        navbar: false
+    },
+    Description
+});

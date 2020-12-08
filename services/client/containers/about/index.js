cbc455e19904f9cb8b6ecedfa5dc0844f256db18
ali-furkqn <mail@alifurkan.codes>
2020-12-08 15:19:12 +0300

    🎊Create (client): About Page

---
+++ services/client/containers/about/index.js
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import { Text } from "@components/text";
+import style from "./about.module.css"
+
+const AboutContainer = ({ title, children }) => {
+    return (
+        <article className={style.hero} >
+            <Text h1 b className={style.title}>
+                {title}
+            </Text>
+            <Text size={"lg"} >{children}</Text>
+        </article>
+    );
+};
+
+AboutContainer.propTypes = {
+    children: PropTypes.any,
+    title: PropTypes.string,
+};
+
+export default AboutContainer;
--- services/client/pages/about.js
+++ services/client/pages/about.js
@@ -1,20 +1,19 @@
 import MessageLayout from "@layouts/message";
+import AboutContainer from "containers/about";
 
 function AboutPage() {
     return (
-        <>
-            You can following this working from{" "}
-            <a
-                className="underline"
-                href="https://github.com/ali-furkqn/Website"
-            >
-                github repo
-            </a>
-        </>
+        <AboutContainer title={"About Me"}>
+            I&apos;m Ali Furkan. I&apos;m full-stack developer who care UI
+            design with Modern Javascript. I have been coding stuff for 5 years.
+            I currently know js,ts,py and beginner level java. I love for Modern
+            Javascript including React,React Native, Mobx, Graphql and Node.
+            Apart from Coding, I like playing gaming, drawing picture and
+            learning something new
+        </AboutContainer>
     );
 }
 
 export default MessageLayout({
-    title: "Not Ready yet",
-    Description: AboutPage,
+    Description: AboutPage
 });

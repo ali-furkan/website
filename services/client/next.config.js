417522113e81f91a5de4ddef999b8ab452ceb557
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-12-08 14:16:59 +0300

    🛠 Configuration (client): Social redirect added

--- services/client/next.config.js
+++ services/client/next.config.js
@@ -8,13 +8,23 @@ module.exports = {
                 permanent: false,
             },
             {
+                source: "/twitter",
+                destination: "https://twitter.com/AliFurkqn",
+                permanent: false,
+            },
+            {
+                source: "/dribbble",
+                destination: "https://dribbble.com/ali_furkan",
+                permanent: false,
+            },
+            {
                 source: "/p/:p",
                 destination: "/projects/:p",
                 permanent: false,
             },
             {
                 source: "/i/:image",
-                destination: "https://cdn.alifurkan.codes/i/:image",
+                destination: "https://a.alifurkan.co/i/:image",
                 permanent: false,
             },
         ];

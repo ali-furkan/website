caff8d22335a0af247794a9472bcfa5277714259
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-12-08 14:20:18 +0300

    🎊 Create (client): "IconButton" component

---
+++ services/client/components/icon/index.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import PropTypes from "prop-types"
+import style from "./style.module.css"
+
+export function IconButton({href, Icon,className, size,...props}) {
+    return (
+        <Link href={href}>
+            <a className={[style.icon,className].join(" ")} tabIndex="0">
+                <Icon fontSize={size} {...props} />
+            </a>
+        </Link>
+    )
+}
+
+IconButton.defaultProps = {
+    size: "2rem"
+}
+
+IconButton.propTypes = {
+    href: PropTypes.string,
+    Icon: PropTypes.any,
+    className: PropTypes.string,
+    size: PropTypes.string
+}

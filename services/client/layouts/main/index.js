4396562f5959afd4332a598761c6932e592c3361
ali-furkqn <mail@alifurkan.codes>
2020-12-17 12:01:32 +0300

    🛠 Edit (client/layouts): navbar's style changed

--- services/client/layouts/main/index.js
+++ services/client/layouts/main/index.js
@@ -5,7 +5,7 @@ import Config from "@config/configuration";
 const MainLayout = (Page) => {
     const PageLayout = (props) => (
         <>
-            <header className={style.header}>
+            <header className={[style.header,style.header_opacity].join(" ")}>
                 <Navbar links={Config.Navbar.links} />
             </header>
             <main className={style.main}>

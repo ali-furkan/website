a5f8cce351b69742b45a0cb89e21eb817b16bc7e
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-12-17 12:02:04 +0300

    🛠 Edit (client/_app): Meta added to website

--- services/client/pages/_app.js
+++ services/client/pages/_app.js
@@ -13,6 +13,13 @@ const MyApp = ({ Component, pageProps }) => {
         <ThemeContext.Provider value={theme}>
             <Head>
                 <title>Ali Furkan - Website</title>
+                <meta name="keywords" content="Ali Furkan, Portfolio" />
+                <meta name="description" content="Ali Furkan's Portfolio" />
+                <meta name="author" content="Ali Furkan" />
+                <meta property="og:title" content="Ali Furkan - Website" />
+                <meta property="og:description" content="Ali Furkan - Website" />
+                <meta name="theme-color" content="#4F2BDF"/>
+                <meta property="og:image" content="https://raw.githubusercontent.com/ali-furkqn/Website/master/assets/repo-banner.png" />
                 <link rel="manifest" href="/manifest.json" />
             </Head>
             <Component {...pageProps} />

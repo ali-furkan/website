4903e1c2f767421a21dc0dcabe50d7076b96cdbb
ali-furkqn <mail@alifurkan.codes>
2020-12-18 14:54:53 +0300

    🛠 Edit (client): About page connected to config

--- services/client/pages/about.js
+++ services/client/pages/about.js
@@ -1,15 +1,11 @@
 import MessageLayout from "@layouts/message";
 import AboutContainer from "containers/about";
+import Config from "@config/configuration"
 
 function AboutPage() {
     return (
-        <AboutContainer title={"About Me"}>
-            I&apos;m Ali Furkan. I&apos;m full-stack developer who care UI
-            design with Modern Javascript. I have been coding stuff for 5 years.
-            I currently know js,ts,py and beginner level java. I love for Modern
-            Javascript including React,React Native, Mobx, Graphql and Node.
-            Apart from Coding, I like playing gaming, drawing picture and
-            learning something new
+        <AboutContainer title={Config.AboutPage.title}>
+            {Config.AboutPage.content}
         </AboutContainer>
     );
 }

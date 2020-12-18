4c5ce6dc8ee1e2cd3819dc7ea17e74a5b9acac30
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-12-18 14:54:53 +0300

    🛠 Edit (client): About page connected to config

--- services/client/config/configuration.js
+++ services/client/config/configuration.js
@@ -20,6 +20,8 @@ export default {
         tags: [
             {
                 label: "Typescript",
+                // bgColor optional
+                // textColor optional
             },
             {
                 label: "React",
@@ -29,4 +31,15 @@ export default {
             }
         ],
     },
+    AboutPage: {
+        title: "About Me",
+        content: `
+        I&apos;m Ali Furkan. I&apos;m full-stack developer who care UI
+        design with Modern Javascript. I have been coding stuff for 5 years.
+        I currently know js,ts,py and beginner level java. I love for Modern
+        Javascript including React,React Native, Mobx, Graphql and Node.
+        Apart from Coding, I like playing gaming, drawing picture and
+        learning something new
+        `
+    }
 };

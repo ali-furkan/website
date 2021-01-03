eea02fc807933c168ef8568c2fe85c12e9865180
ali-furkqn <mail@alifurkan.codes>
2021-01-03 17:03:54 +0300

    :rotating_light: (Components): About Container Formatted

--- services/client/containers/about/index.js
+++ services/client/containers/about/index.js
@@ -1,14 +1,14 @@
 import PropTypes from "prop-types";
 import { Text } from "@components/text";
-import style from "./about.module.css"
+import style from "./about.module.css";
 
 const AboutContainer = ({ title, children }) => {
     return (
-        <article className={style.hero} >
+        <article className={style.hero}>
             <Text h1 b className={style.title}>
                 {title}
             </Text>
-            <Text size={"lg"} >{children}</Text>
+            <Text size={"lg"}>{children}</Text>
         </article>
     );
 };

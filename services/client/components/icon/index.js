fb8e8b5e37a9dc22ddbd4ab1cb67c99d522df296
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2021-01-03 16:37:16 +0300

    :bulb: (Components): Comment added for Icon Component

--- services/client/components/icon/index.js
+++ services/client/components/icon/index.js
@@ -1,24 +1,32 @@
 import Link from "next/link";
-import PropTypes from "prop-types"
-import style from "./style.module.css"
+import PropTypes from "prop-types";
+import style from "./style.module.css";
 
-export function IconButton({href, Icon,className, size,...props}) {
+/**
+ *
+ * @param {{ href: string; Icon:import("react").Component; className: string; size: string;}} param0
+ */
+export function IconButton({ href, Icon, className, size, ...props }) {
     return (
         <Link href={href}>
-            <a title={href} className={[style.icon,className].join(" ")} tabIndex="0">
+            <a
+                title={href}
+                className={[style.icon, className].join(" ")}
+                tabIndex="0"
+            >
                 <Icon fontSize={size} {...props} />
             </a>
         </Link>
-    )
+    );
 }
 
 IconButton.defaultProps = {
-    size: "2rem"
-}
+    size: "2rem",
+};
 
 IconButton.propTypes = {
     href: PropTypes.string,
     Icon: PropTypes.any,
     className: PropTypes.string,
-    size: PropTypes.string
-}
+    size: PropTypes.number,
+};

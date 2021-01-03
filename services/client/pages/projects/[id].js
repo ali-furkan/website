c5477bf9d7be153b01210ef926c8adaab5293010
ali-furkqn <mail@alifurkan.codes>
2021-01-03 18:16:39 +0300

    :recycle: (Pages): Refactored Post Pages
    Project/[id] and Blogs/[id] Refactored

--- services/client/pages/projects/[id].js
+++ services/client/pages/projects/[id].js
@@ -3,10 +3,15 @@ import React from "react";
 import { useRouter } from "next/router";
 import renderToString from "next-mdx-remote/render-to-string";
 import hydrate from "next-mdx-remote/hydrate";
+import matter from "gray-matter";
+import Article from "containers/article";
 import MainLayout from "@layouts/main";
 import { MdxPageHead } from "@components/mdx-header";
+import { MdxPageText } from "@components/mdx-text";
 import { Text } from "@components/text";
-import { Container } from "@components/container";
+import config from "@config/index";
+
+const components = { MdxPageHead, MdxPageText, Text };
 
 const ProjectPage = ({ source }) => {
     const router = useRouter();
@@ -14,14 +19,14 @@ const ProjectPage = ({ source }) => {
         if (!source) return router.push("/projects");
     });
     if (!source) return <h1>Page not found</h1>;
-    const content = hydrate(source, { MdxPageHead, Text });
+    const content = hydrate(source, { components });
 
-    return <Container>{content}</Container>;
+    return <Article>{content}</Article>;
 };
 
 export const getStaticPaths = async () => {
     try {
-        const res = await fetch(process.env.STORAGE_BASE_URL + "/project-metas", {
+        const res = await fetch(config.baseUrl + "/projects-metas", {
             headers: {
                 Authorization: process.env.STORAGE_TOKEN,
             },
@@ -29,43 +34,45 @@ export const getStaticPaths = async () => {
 
         if (!res.ok) throw new Error(res.statusText);
 
-        const blogs = await res.json();
+        const data = await res.json();
 
-        const paths = blogs.map((b) => ({
-            params: { id: b.href.query.id },
-        }));
+        const paths = await Promise.all(
+            data.map(async (b) => {
+                const res = await fetch(b.url);
+                if (!res.ok) return null;
+                const d = await res.json();
+                return { params: { id: d?.href?.query?.id } };
+            })
+        );
 
-        return { paths, fallback: true };
+        return { paths, fallback: false };
     } catch (e) {
-        console.log("Project Page", e);
+        console.debug("Blog Page getStaticPaths", e);
         return { paths: [], fallback: true };
     }
 };
 
 export const getStaticProps = async ({ params }) => {
     try {
-        const res = await fetch(
-            process.env.STORAGE_BASE_URL + "/projects/" + params.id
-        );
+        const res = await fetch(config.baseUrl + "/projects/" + params.id);
         if (!res.ok) throw new Error(res.statusText);
         const source = await res.text();
-        const mdxSource = await renderToString(source, {
-            components: { MdxPageHead, Text },
+        const { content, data } = matter(source);
+        const mdxSource = await renderToString(content, {
+            components,
+            scope: data,
         });
 
         return {
-            revalidate: 30,
+            revalidate: 60,
             props: {
                 source: mdxSource || "",
             },
         };
     } catch (e) {
         return {
-            revalidate: 30,
-            redirect: {
-                destination: "/",
-                permanent: false,
-            },
+            notFound: true,
+            revalidate: 5,
             props: {
                 source: "",
             },

ea93caf6a83896f11fe80532a1bd815d433cc900
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2021-01-03 16:33:07 +0300

    :art: (Components): Card Component is appropriating for Modularity
    Card Component is appropriating for Modularity

--- services/client/components/card/index.js
+++ services/client/components/card/index.js
@@ -2,11 +2,30 @@ import PropTypes, { object } from "prop-types";
 import Link from "next/link";
 import { Text } from "@components/text";
 import style from "./card.module.css";
+import styleRow from "./card-row.module.css";
+import { Skeleton } from "@components/skeleton";
 
-export const Card = ({ title, image, tags, date, href }) => {
+export const CardFooter = ({ children }) => (
+    <footer className="border-t-2 border-primary-200 p-5">{children}</footer>
+);
+
+CardFooter.propTypes = {
+    children: PropTypes.any,
+};
+
+const propTypes = {
+    image: PropTypes.string,
+    title: PropTypes.string,
+    tags: PropTypes.arrayOf(object),
+    date: PropTypes.string,
+    footer: PropTypes.any,
+};
+
+const DefaultCard = ({ title, image, tags, date, footer }) => {
     return (
-        <Link href={href}>
-            <article className={style.container}>
+        <article className={style.container}>
+            {!image && <Skeleton type="image" size={"xl"} />}
+            {image && (
                 <div className={style.img_wrapper}>
                     <img
                         alt={`img-${title.toLowerCase()}`}
@@ -14,30 +33,92 @@ export const Card = ({ title, image, tags, date, href }) => {
                         width="256px"
                     />
                 </div>
-                <footer className={style.info}>
-                    <div className={style.tag_grid}>
-                        {tags.map((t, i) => (
+            )}
+            <div className={style.info}>
+                <div className={style.tag_grid}>
+                    {tags &&
+                        tags.map((t, i) => (
                             <Text key={i} {...t} b>
                                 {t?.text}
                             </Text>
                         ))}
-                    </div>
-                    <Text className={style.title} h3 b>
-                        {title}
-                    </Text>
-                    <Text color={"primary-100"} size={"sm"} b>
-                        {date}
-                    </Text>
-                </footer>
-            </article>
-        </Link>
+                </div>
+                <Text className={style.title} h3 b>
+                    {title}
+                </Text>
+                <Text color={"primary-100"} size={"sm"} b>
+                    {date}
+                </Text>
+            </div>
+            {footer && <CardFooter>{footer}</CardFooter>}
+        </article>
     );
 };
 
+DefaultCard.propTypes = propTypes;
+
+export const RowCard = ({ image, title, tags, date, footer }) => {
+    return (
+        <article className={styleRow.container}>
+            {!image && <Skeleton type="image" size={"xl"} />}
+            {image && (
+                <div className={styleRow.img_wrapper}>
+                    <img
+                        alt={`img-${title.toLowerCase()}`}
+                        src={image}
+                        className="max-h-24 max-w-24"
+                    />
+                </div>
+            )}
+            <div className="flex flex-col mx-4">
+                <Text h3 b>
+                    {title}
+                </Text>
+                <Text color={"primary-100"} size={"sm"} b>
+                    {date}
+                </Text>
+                <div className={style.tag_grid}>
+                    {tags &&
+                        tags.map((t, i) => (
+                            <Text key={i} {...t} p b>
+                                {t?.text}
+                            </Text>
+                        ))}
+                </div>
+            </div>
+            {footer && <CardFooter>{footer}</CardFooter>}
+        </article>
+    );
+};
+
+RowCard.propTypes = propTypes;
+
+/**
+ * Card Component
+ * @param {{ theme: "default"|"row"; href: string; image: string; title: string; tags: string; date:string}} param0
+ */
+export const Card = ({ theme, href, ...props }) => {
+    const Component = (props) => {
+        if (theme === "default") return <DefaultCard {...props} />;
+        if (theme === "row") return <RowCard {...props} />;
+    };
+
+    if (href)
+        return (
+            <Link href={href}>
+                <a tabIndex="0">
+                    <Component {...props} />
+                </a>
+            </Link>
+        );
+    return <Component {...props} />;
+};
+
+Card.defaultProps = {
+    theme: "default",
+};
+
 Card.propTypes = {
-    image: PropTypes.string,
-    title: PropTypes.string,
-    tags: PropTypes.arrayOf(object),
-    date: PropTypes.string,
     href: PropTypes.any,
+    theme: PropTypes.string,
 };

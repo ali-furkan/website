d3aeed01991780ca078089b577ba8a2e667d4cdb
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2021-01-03 17:18:33 +0300

    :wrench: (Client): Configuration File Created In Root Path

---
+++ services/client/web.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    Navbar: {
+        logo: {
+            light: "/assets/logo-black.png",
+            dark: "/assets/logo-white.png",
+        },
+        links: {
+            About: {
+                text: "About Me",
+                href: "about",
+            },
+            Blogs: "blogs",
+            Projects: "projects",
+            Github: "github",
+        },
+    },
+    HomePage: {
+        title: "ali furkan",
+        subTitle: "a developer",
+        tags: [
+            {
+                label: "Typescript",
+            },
+            {
+                label: "React",
+            },
+            {
+                label: "Nest",
+            },
+        ],
+    },
+    AboutPage: {
+        title: "About Me",
+        content:
+            "I'm Ali Furkan. I'm full-stack developer who care UI design with Modern Javascript. I have been coding stuff for 5 years. I'm currently interested js, ts, golang, C++. I love for Modern Javascript including React, React Native, Mobx, Graphql and Node. Apart from Coding, I like playing gaming, drawing picture and learning something new",
+    },
+    StorageDomain: "s.alifurkan.co",
+    PublishDomain: "alifurkan.co",
+    SocialLinks: {
+        github: "https://github.com/ali-furkqn",
+        twitter: "https://twitter.com/AliFurkqn",
+        dribble: "https://dribbble.com/ali_furkan",
+    },
+};

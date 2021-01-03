c4de3effe92dcd9178e1634325a75d5704e2f0b2
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2021-01-03 17:05:01 +0300

    :sparkles: (Containers): Article Container Created

---
+++ services/client/containers/article/index.js
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import styles from "./article.module.css";
+
+function Article({ children }) {
+    return <div className={styles.main}>{children}</div>;
+}
+
+Article.propTypes = {
+    children: PropTypes.any.isRequired,
+};
+
+export default Article;

590d4ebd6a222ff72f1b2a0caf4700c7636af9e6
ali-furkqn <mail@alifurkan.codes>
2021-01-03 17:38:03 +0300

    :sparkles: (Libs): Theme Hook Created

--- services/client/lib/theme.js
+++ services/client/lib/theme.js
@@ -1,24 +1,20 @@
-import {createContext, useState, useEffect, useContext} from "react"
-
-export const ThemeContext = createContext("light")
+import React from "react";
+import { ThemeContext } from "contexts/theme";
 
 export const useTheme = () => {
-    const [theme, setTheme] = useState("light")
-    const ctxTheme = useContext(ThemeContext)
-    
-    useEffect(()=>{
+    const [theme, setTheme] = React.useState("light");
+    const ctxTheme = React.useContext(ThemeContext);
+
+    React.useEffect(() => {
         const themeHandler = () => {
-            if(!window.matchMedia) 
-                return setTheme("dark")
-            const themeQuery = s => window.matchMedia(`(prefers-color-scheme: ${s})`).matches
-            if(themeQuery("light"))
-                return setTheme("light")
-            if(themeQuery("dark"))
-                return setTheme("dark")
-            if(typeof ctxTheme === "string")
-                return setTheme(ctxTheme)
-        }
-        themeHandler()
-    },[theme])
-    return [theme,setTheme]
-}
+            if (!window.matchMedia) return setTheme("dark");
+            const themeQuery = (s) =>
+                window.matchMedia(`(prefers-color-scheme: ${s})`).matches;
+            if (themeQuery("light")) return setTheme("light");
+            if (themeQuery("dark")) return setTheme("dark");
+            if (typeof ctxTheme === "string") return setTheme(ctxTheme);
+        };
+        themeHandler();
+    }, [theme]);
+    return [theme, setTheme];
+};

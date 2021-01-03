5492094939042582a9b2db6dbe5ce310ff127d86
ali-furkqn <mail@alifurkan.codes>
2021-01-03 16:47:09 +0300

    :rotating_light: (Components): Row Component Reformatted

--- services/client/components/row/index.js
+++ services/client/components/row/index.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import style from "./row.module.css"
+import style from "./row.module.css";
 
 export const Row = ({
     children,
@@ -8,27 +8,22 @@ export const Row = ({
     alignContent,
     direction,
 }) => {
-
     const classNames = [
-        justify?style[`j-${justify}`]:undefined,
-        aligntItems?style[`i-${aligntItems}`]:undefined,
-        alignContent?style[`c-${alignContent}`]:undefined,
-        direction?style[direction]:undefined
-    ].filter(p=>p)
+        justify ? style[`j-${justify}`] : undefined,
+        aligntItems ? style[`i-${aligntItems}`] : undefined,
+        alignContent ? style[`c-${alignContent}`] : undefined,
+        direction ? style[direction] : undefined,
+    ].filter((p) => p);
 
-    return (
-        <div className={classNames.join(" ")}>
-            {children}
-        </div>
-    )
-}
+    return <div className={classNames.join(" ")}>{children}</div>;
+};
 
 Row.defaultProps = {
     justify: "flex-start",
     aligntItems: "stretch",
     alignContent: "flex-start",
-    direction: "flex-start"
-}
+    direction: "flex-start",
+};
 
 Row.propTypes = {
     children: PropTypes.any,
@@ -36,4 +31,4 @@ Row.propTypes = {
     aligntItems: PropTypes.string,
     alignContent: PropTypes.string,
     direction: PropTypes.string,
-}
+};

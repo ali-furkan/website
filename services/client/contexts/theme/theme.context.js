eb68049cfd56223e681e2be4916df9cf0fa8c2c9
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2021-01-03 17:36:19 +0300

    :sparkles: (Contexts): Theme Context Created

---
+++ services/client/contexts/theme/theme.context.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+import { useTheme } from "@lib/theme";
+
+export const ThemeContext = React.createContext("light");
+
+export const ThemeProvider = ({ children }) => {
+    const [theme] = useTheme();
+    return (
+        <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
+    );
+};

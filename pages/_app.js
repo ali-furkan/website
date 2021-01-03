9f3fa4c7707414890692ba84eb4947ea54224ba0
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2021-01-03 23:14:57 +0300

    :mag: (Pages): Google Analytics and Twitter Meta Tags Added

--- pages/_app.js
+++ pages/_app.js
@@ -19,24 +19,76 @@ const MyApp = ({ Component, pageProps }) => {
         <ThemeProvider>
             <AuthProvider>
                 <Head>
+                    <link rel="manifest" href="/manifest.json" />
+
                     <title>{webConfig.Meta.title}</title>
+
+                    {/* Meta Tags */}
                     <meta name="theme-color" content={webConfig.Meta.color} />
                     <meta name="author" content={webConfig.Meta.author} />
                     <meta name="keywords" content={webConfig.Meta.keywords} />
-                    <meta property="og:title" content={webConfig.Meta.title} />
-                    <meta property="og:url" content={webConfig.PublishDomain} />
-                    <meta property="og:image" content={webConfig.Meta.image} />
-                    <meta property="og:og:locale" content="en_GB" />
-                    <meta property="og:og:locale:alternate" content="tr_TR" />
                     <meta
                         name="description"
                         content={webConfig.Meta.description}
                     />
+
+                    {/* Open Graph */}
+                    <meta property="og:title" content={webConfig.Meta.title} />
                     <meta
                         property="og:description"
                         content={webConfig.Meta.description}
                     />
-                    <link rel="manifest" href="/manifest.json" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content={webConfig.PublishDomain} />
+                    <meta
+                        property="og:site_name"
+                        content={webConfig.Meta.author}
+                    />
+                    <meta
+                        property="og:image"
+                        content={`https://${webConfig.PublishDomain}${webConfig.Meta.image}`}
+                    />
+                    <meta property="og:locale" content="en_GB" />
+                    <meta property="og:locale:alternate" content="tr_TR" />
+
+                    {/* Twitter */}
+                    <meta
+                        property="twitter:url"
+                        content={webConfig.PublishDomain}
+                    />
+                    <meta
+                        property="twitter:title"
+                        content={webConfig.Meta.title}
+                    />
+                    <meta
+                        property="twitter:description"
+                        content={webConfig.Meta.description}
+                    />
+                    <meta
+                        property="twitter:image"
+                        content={`https://${webConfig.PublishDomain}${webConfig.Meta.image}`}
+                    />
+
+                    <meta
+                        property="twitter:card"
+                        content={`https://${webConfig.PublishDomain}${webConfig.Meta.image}`}
+                    />
+
+                    {/* Google Analytics */}
+                    <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=UA-134822812-3"
+                    ></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', 'UA-134822812-3');
+                            `,
+                        }}
+                    />
                 </Head>
                 <Component {...pageProps} />
                 <Toasty />

1c51540ad6fc74cacdc7b5996e8028c08041c7ac
ali-furkqn <mail@alifurkan.codes>
2021-01-03 18:15:13 +0300

    :recycle: (Pages): _app Pages Refactored with Contexts

--- services/client/pages/_app.js
+++ services/client/pages/_app.js
@@ -1,30 +1,44 @@
 /* eslint-disable react/prop-types */
 import Head from "next/head";
+import { AuthProvider } from "contexts/auth";
+import { ThemeProvider } from "contexts/theme";
+import config from "@config/index";
+import { Toasty } from "@components/toasty";
+
 import "@styles/tailwind.css";
-import {ThemeContext, useTheme} from "@lib/theme"
+import "@styles/toast.css";
+import "react-toastify/dist/ReactToastify.css";
+
 export function reportWebVitals(metric) {
-    console.log(metric);
+    if (!config.isProd) console.log(metric);
 }
 
 const MyApp = ({ Component, pageProps }) => {
-    const [theme] = useTheme()
-    
     return (
-        <ThemeContext.Provider value={theme}>
-            <Head>
-                <title>Ali Furkan - Website</title>
-                <meta name="keywords" content="Ali Furkan, Portfolio" />
-                <meta name="description" content="Ali Furkan's Portfolio" />
-                <meta name="author" content="Ali Furkan" />
-                <meta property="og:title" content="Ali Furkan - Website" />
-                <meta property="og:description" content="Ali Furkan - Website" />
-                <meta name="theme-color" content="#4F2BDF"/>
-                <meta property="og:image" content="https://raw.githubusercontent.com/ali-furkqn/Website/master/assets/repo-banner.png" />
-                <link rel="manifest" href="/manifest.json" />
-            </Head>
-            <Component {...pageProps} />
-        </ThemeContext.Provider>
+        <ThemeProvider>
+            <AuthProvider>
+                <Head>
+                    <title>Ali Furkan - Website</title>
+                    <meta name="keywords" content="Ali Furkan, Portfolio" />
+                    <meta name="description" content="Ali Furkan's Portfolio" />
+                    <meta name="author" content="Ali Furkan" />
+                    <meta property="og:title" content="Ali Furkan - Website" />
+                    <meta
+                        property="og:description"
+                        content="Ali Furkan - Website"
+                    />
+                    <meta name="theme-color" content="#4F2BDF" />
+                    <meta
+                        property="og:image"
+                        content="https://raw.githubusercontent.com/ali-furkqn/Website/master/assets/repo-banner.png"
+                    />
+                    <link rel="manifest" href="/manifest.json" />
+                </Head>
+                <Component {...pageProps} />
+                <Toasty />
+            </AuthProvider>
+        </ThemeProvider>
     );
-} 
+};
 
 export default MyApp;

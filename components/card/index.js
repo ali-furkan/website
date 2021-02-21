7f433f2faa36fe38e3ba6881aaa8883c96a0b973
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2021-02-21 23:46:32 +0300

    ⚡️ (navbar): converted logo svg

--- components/card/index.js
+++ components/card/index.js
@@ -6,89 +6,85 @@ import style from "./card.module.css";
 import styleRow from "./card-row.module.css";
 
 export const CardFooter = ({ children }) => (
-    <footer className="border-t-2 border-primary-200 p-5">{children}</footer>
+	<footer className="border-t-2 border-primary-200 p-5">{children}</footer>
 );
 
 CardFooter.propTypes = {
-    children: PropTypes.any,
+	children: PropTypes.any,
 };
 
 const propTypes = {
-    image: PropTypes.string,
-    title: PropTypes.string,
-    tags: PropTypes.arrayOf(PropTypes.object),
-    date: PropTypes.string,
-    footer: PropTypes.any,
+	image: PropTypes.string,
+	title: PropTypes.string,
+	tags: PropTypes.arrayOf(PropTypes.object),
+	date: PropTypes.string,
+	footer: PropTypes.any,
 };
 
 const DefaultCard = ({ title, image, tags, date, footer }) => {
-    return (
-        <article className={style.container}>
-            {!image && <Skeleton type="image" size={"xl"} />}
-            {image && (
-                <div className={style.img_wrapper}>
-                    <img
-                        alt={`img-${title.toLowerCase()}`}
-                        src={image}
-                        width="256px"
-                    />
-                </div>
-            )}
-            <div className={style.info}>
-                <div className={style.tag_grid}>
-                    {tags &&
-                        tags.map((t, i) => (
-                            <Text key={i} {...t} b>
-                                {t?.text}
-                            </Text>
-                        ))}
-                </div>
-                <Text className={style.title} h3 b>
-                    {title}
-                </Text>
-                <Text color={"primary-100"} size={"sm"} b>
-                    {date}
-                </Text>
-            </div>
-            {footer && <CardFooter>{footer}</CardFooter>}
-        </article>
-    );
+	return (
+		<article className={style.container}>
+			{!image && <Skeleton type="image" size={"xl"} />}
+			{image && (
+				<div className={style.img_wrapper}>
+					<img alt={`img-${title.toLowerCase()}`} src={image} width="256px" />
+				</div>
+			)}
+			<div className={style.info}>
+				<div className={style.tag_grid}>
+					{tags &&
+						tags.map((t, i) => (
+							<Text key={i} {...t} b>
+								{t?.text}
+							</Text>
+						))}
+				</div>
+				<Text className={style.title} h3 b>
+					{title}
+				</Text>
+				<Text color={"primary-100"} size={"sm"} b>
+					{date}
+				</Text>
+			</div>
+			{footer && <CardFooter>{footer}</CardFooter>}
+		</article>
+	);
 };
 
 DefaultCard.propTypes = propTypes;
 
 export const RowCard = ({ image, title, tags, date, footer }) => {
-    return (
-        <article className={styleRow.container}>
-            {!image && <Skeleton type="image" size={"xl"} />}
-            {image && (
-                <div className={styleRow.img_wrapper}>
-                    <img
-                        alt={`img-${title.toLowerCase()}`}
-                        src={image}
-                        className="max-h-24 max-w-24"
-                    />
-                </div>
-            )}
-            <div className="flex flex-col mx-4">
-                <Text h3 b>
-                    {title}
-                </Text>
-                <Text color={"primary-100"} size={"sm"} b>
-                    {date}
-                </Text>
-                <div className={style.tag_grid}>
-                    {tags &&
-                        tags.map((t, i) => (
-                            <Text key={i} {...t} p b>
-                                {t?.text}
-                            </Text>
-                        ))}
-                </div>
-            </div>
-            {footer && <CardFooter>{footer}</CardFooter>}
-        </article>
-    );
+	return (
+		<article className={styleRow.container}>
+			{!image && <Skeleton type="image" size={"xl"} />}
+			{image && (
+				<div className={styleRow.img_wrapper}>
+					<img
+						alt={`img-${title.toLowerCase()}`}
+						src={image}
+						className="max-h-24 max-w-24"
+					/>
+				</div>
+			)}
+			<div className="flex flex-col mx-4">
+				<Text h3 b>
+					{title}
+				</Text>
+				<Text color={"primary-100"} size={"sm"} b>
+					{date}
+				</Text>
+				<div className={style.tag_grid}>
+					{tags &&
+						tags.map((t, i) => (
+							<Text key={i} {...t} p b>
+								{t?.text}
+							</Text>
+						))}
+				</div>
+			</div>
+			{footer && <CardFooter>{footer}</CardFooter>}
+		</article>
+	);
 };
 
 RowCard.propTypes = propTypes;
@@ -98,27 +94,27 @@ RowCard.propTypes = propTypes;
  * @param {{ theme: "default"|"row"; href: string; image: string; title: string; tags: string; date:string}} param0
  */
 export const Card = ({ theme, href, ...props }) => {
-    const Component = (props) => {
-        if (theme === "default") return <DefaultCard {...props} />;
-        if (theme === "row") return <RowCard {...props} />;
-    };
+	const Component = (props) => {
+		if (theme === "default") return <DefaultCard {...props} />;
+		if (theme === "row") return <RowCard {...props} />;
+	};
 
-    if (href)
-        return (
-            <Link href={href}>
-                <a tabIndex="0">
-                    <Component {...props} />
-                </a>
-            </Link>
-        );
-    return <Component {...props} />;
+	if (href)
+		return (
+			<Link href={href}>
+				<a tabIndex="0">
+					<Component {...props} />
+				</a>
+			</Link>
+		);
+	return <Component {...props} />;
 };
 
 Card.defaultProps = {
-    theme: "default",
+	theme: "default",
 };
 
 Card.propTypes = {
-    href: PropTypes.any,
-    theme: PropTypes.string,
+	href: PropTypes.any,
+	theme: PropTypes.string,
 };

d3c95a9ba9d505393a24572f283b3efb7ac9977e
ali-furkqn <a.furkan.2003@gmail.com>
2021-02-22 00:48:01 +0300

    💄 (container/home): Adapted home for colorless ui

--- containers/home/index.js
+++ containers/home/index.js
@@ -1,60 +1,38 @@
 import PropTypes from "prop-types";
-import { useTheme } from "@/lib/theme";
+
 import { IconButton } from "@/components/icon";
-import { Tag } from "@/components/tag";
 import { Text } from "@/components/text";
+
 import style from "./home.module.css";
 
-const HomeContainer = ({ title, subTitle, tags, links }) => {
-    const [theme] = useTheme();
-
-    return (
-        <div className={style.hero}>
-            <Text h4 b color={"secondary-300"}>
-                Hello I&apos;m
-            </Text>
-            <div className={style.name_title}>
-                <Text h1 b>
-                    {title}
-                </Text>
-                <Text
-                    h3
-                    color={theme === "dark" ? "secondary-200" : "secondary-400"}
-                    size={"lg"}
-                >
-                    {subTitle}
-                </Text>
-            </div>
-            <div className={style.tag_list}>
-                {tags.map((t, k) => (
-                    <Tag
-                        key={k}
-                        className={style.tag}
-                        label={t.label}
-                        bgColor={t.bgColor}
-                        textColor={t.textColor}
-                    />
-                ))}
-            </div>
-            <div className={style.icon_list}>
-                {links.map((i, k) => (
-                    <IconButton key={k} {...i} />
-                ))}
-            </div>
-        </div>
-    );
+const HomeContainer = ({ title, subTitle, links }) => {
+	return (
+		<div className={style.hero}>
+			<Text size="6xl">
+				Hi! I&apos;m <Text b>{title}</Text>
+			</Text>
+			<Text size="xl" italic>
+				{subTitle}
+			</Text>
+			<div className={style.icon_list}>
+				{links.map((i, k) => (
+					<IconButton key={k} {...i} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 HomeContainer.defaultProps = {
-    tags: [],
-    links: [],
+	tags: [],
+	links: [],
 };
 
 HomeContainer.propTypes = {
-    title: PropTypes.string,
-    subTitle: PropTypes.string,
-    tags: PropTypes.arrayOf(PropTypes.object).isRequired,
-    links: PropTypes.arrayOf(PropTypes.object).isRequired,
+	title: PropTypes.string,
+	subTitle: PropTypes.string,
+	tags: PropTypes.arrayOf(PropTypes.object).isRequired,
+	links: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default HomeContainer;

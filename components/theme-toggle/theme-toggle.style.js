8269d68c4218625cb39061db66a1aae3e163a285
ali-furkqn <a.furkan.2003@gmail.com>
2021-05-05 11:24:51 +0300

    :sparkles: (components): Created theme-toggle

---
+++ components/theme-toggle/theme-toggle.style.js
@@ -0,0 +1,10 @@
+import tw from "twin.macro"
+
+export const StyledThemeToggle = tw.div`
+    rounded-lg
+    bg-gray-200 dark:bg-gray-800
+    p-2.5
+    cursor-pointer
+    ease-in transition-all 
+    hover:bg-opacity-70
+`

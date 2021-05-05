f19f31e4e65f06dc053778ff73267a641f0d4979
ali-furkqn <a.furkan.2003@gmail.com>
2021-05-05 11:23:02 +0300

    :recycle: (components): Refac components

--- components/card/index.js
+++ components/card/index.js
@@ -1,120 +1,39 @@
-import PropTypes from "prop-types";
-import Link from "next/link";
-import { Text } from "@/components/text";
-import { Skeleton } from "@/components/skeleton";
-import style from "./card.module.css";
-import styleRow from "./card-row.module.css";
+import Link from "next/link"
+import {
+	StyledCard,
+	StyledCardContainer,
+	StyledCardContent,
+	StyledCardFooter,
+	StyledImage
+} from "./card.style"
 
-export const CardFooter = ({ children }) => (
-	<footer className="border-t-2 border-primary-200 p-5">{children}</footer>
-);
-
-CardFooter.propTypes = {
-	children: PropTypes.any,
-};
-
-const propTypes = {
-	image: PropTypes.string,
-	title: PropTypes.string,
-	tags: PropTypes.arrayOf(PropTypes.object),
-	date: PropTypes.string,
-	footer: PropTypes.any,
-};
-
-const DefaultCard = ({ title, image, tags, date, footer }) => {
-	return (
-		<article className={style.container}>
-			{!image && <Skeleton type="image" size={"xl"} />}
-			{image && (
-				<div className={style.img_wrapper}>
-					<img alt={`img-${title.toLowerCase()}`} src={image} width="256px" />
-				</div>
-			)}
-			<div className={style.info}>
-				<div className={style.tag_grid}>
-					{tags &&
-						tags.map((t, i) => (
-							<Text key={i} {...t} b>
-								{t?.text}
-							</Text>
-						))}
-				</div>
-				<Text className={style.title} h3 b>
-					{title}
-				</Text>
-				<Text color={"primary-100"} size={"sm"} b>
-					{date}
-				</Text>
-			</div>
-			{footer && <CardFooter>{footer}</CardFooter>}
-		</article>
-	);
-};
-
-DefaultCard.propTypes = propTypes;
-
-export const RowCard = ({ image, title, tags, date, footer }) => {
-	return (
-		<article className={styleRow.container}>
-			{!image && <Skeleton type="image" size={"xl"} />}
-			{image && (
-				<div className={styleRow.img_wrapper}>
-					<img
-						alt={`img-${title.toLowerCase()}`}
+function Card({ image, children, footer, href }) {
+	const component = (
+		<StyledCard tabIndex="0">
+			<StyledCardContainer>
+				{image && (
+					<StyledImage
+						alt={`img-${image.split("/").pop().toLowerCase()}`}
 						src={image}
-						className="max-h-24 max-w-24"
 					/>
-				</div>
-			)}
-			<div className="flex flex-col mx-4">
-				<Text h3 b>
-					{title}
-				</Text>
-				<Text color={"primary-100"} size={"sm"} b>
-					{date}
-				</Text>
-				<div className={style.tag_grid}>
-					{tags &&
-						tags.map((t, i) => (
-							<Text key={i} {...t} p b>
-								{t?.text}
-							</Text>
-						))}
-				</div>
-			</div>
-			{footer && <CardFooter>{footer}</CardFooter>}
-		</article>
-	);
-};
+				)}
+				<StyledCardContent aria-label="card-content">
+					{children}
+				</StyledCardContent>
+			</StyledCardContainer>
+			{footer && <StyledCardFooter>{footer} </StyledCardFooter>}
+		</StyledCard>
+	)
 
-RowCard.propTypes = propTypes;
-
-/**
- * Card Component
- * @param {{ theme: "default"|"row"; href: string; image: string; title: string; tags: string; date:string}} param0
- */
-export const Card = ({ theme, href, ...props }) => {
-	const Component = (props) => {
-		if (theme === "default") return <DefaultCard {...props} />;
-		if (theme === "row") return <RowCard {...props} />;
-	};
-
-	if (href)
+	if (href) {
 		return (
-			<Link href={href}>
-				<a tabIndex="0">
-					<Component {...props} />
-				</a>
+			<Link href={href} passHref>
+				{component}
 			</Link>
-		);
-	return <Component {...props} />;
-};
+		)
+	}
 
-Card.defaultProps = {
-	theme: "default",
-};
+	return component
+}
 
-Card.propTypes = {
-	href: PropTypes.any,
-	theme: PropTypes.string,
-};
+export default Card
---
+++ components/navbar/navbar.style.js
@@ -0,0 +1,32 @@
+import tw from "twin.macro"
+
+export const StyledHeader = tw.header`
+    backdrop-filter[blur(8px)]
+    bg-white
+    dark:bg-black
+    dark:bg-opacity-20
+    bg-opacity-20
+    w-full
+    sticky
+    top-0
+    z-10
+`
+
+export const StyledNavbar = tw.nav`
+    max-w-5xl
+    mx-auto
+    flex justify-between items-center
+    py-5 px-6
+    select-none
+`
+
+export const StyledNavbarLinks = tw.div`flex items-center`
+
+export const StyledNavbarLink = tw.a`
+    cursor-pointer 
+    px-2.5 sm:px-5
+    h-full
+    dark:text-gray-100
+    duration-200 ease-in transition-all 
+    hover:opacity-60
+`
